107079ea4474041061454dcd9a41420456cce453
saka-abdulrahman <c.abdulrahmansaka@gmail.com>
2024-08-27 13:46:42 +0300

    Resposive designs added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,11 +20,11 @@ const Home = () => {
   return (
     <div className={` ${isDark ? "dark" : ""}`}>
       <Header isDark={isDark} setIsDark={setIsDark} />
-      <div className="bg-bg2 dark:bg-bg2Dark   min-h-[100vh] pt-20 flex justify-evenly ">
-        <div></div>
-        <div></div>
+      <div className="bg-bg2 dark:bg-bg2Dark   min-h-[100vh] pt-20 max-[700px]:pt-7 flex justify-evenly items-c ">
+        <div className="max-[700px]:hidden "></div>
+        <div className="max-[700px]:hidden "></div>
         <Posts />
-        <div></div>
+        <div className="max-[700px]:hidden "></div>
       </div>
       <Menu />
     </div>
